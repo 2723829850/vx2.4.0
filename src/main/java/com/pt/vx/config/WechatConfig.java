456465c2501614217755a869cad130a2f7df132b
2723829850 <42460775+2723829850@users.noreply.github.com>
2023-05-06 17:20:47 +0800

    Update WechatConfig.java

--- src/main/java/com/pt/vx/config/WechatConfig.java
+++ src/main/java/com/pt/vx/config/WechatConfig.java
@@ -38,15 +38,15 @@ public class WechatConfig {
      */
     static {
         userList.add(getUser(
-                "这个人扫码后的微信号", //扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "微信消息模板ID", //要给这个人发送的模板ID
-                "pt", //咋称呼这个人
-                "江苏省南京市玄武区", //这个人的详细地址
-                "南京", //这个人在的城市
-                new BirthDay(1999,2,15,true,false,"pt生日快乐！！"),
+                "oiJVP5mZKyyxpPRt2WTn9i3vdP1w", //扫码关注你的测试号以后，测试平台会出现TA的微信号
+                "TWIge5TTvayrPGRSWGgz67Kok2IsHbXZt-oLWEdXQfg", //要给这个人发送的模板ID
+                "宝贝", //咋称呼这个人
+                "江苏省徐州市睢宁县", //这个人的详细地址
+                "徐州", //这个人在的城市
+                new BirthDay(2002,1,2,true,false,"pt生日快乐！！"),
                 new BirthDay(1999,8,11,false,false,"生日快乐哦~~"),
                 new BirthDay(2020,7,8,true,true),
-                new BirthDay(2020,7,8,true,false,"周年快乐！！！")
+                new BirthDay(2022,10,3,true,false,"周年快乐！！！")
         ));
 
         userList.add(getUser(
